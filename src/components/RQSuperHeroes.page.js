8a71cee200ab116c8e9f1bb54db5e70484847e40
Dmytro Poliuliuk <dpoli@ciklum.com>
2022-01-29 22:17:29 +0200

    refetch

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useQuery } from "react-query";
+import { useIsFetching, useQuery } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeroes = () => {
@@ -7,15 +7,32 @@ const fetchSuperHeroes = () => {
 };
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery("super-heroes", fetchSuperHeroes);
+  const { isLoading, data, isError, error, refetch, isFetching } = useQuery(
+    "super-heroes",
+    fetchSuperHeroes,
+    {
+      // cacheTime: 5000,
+      // staleTime: 30000,
+      // refetchOnMount: false,
+      // refetchOnWindowFocus: 'always',
+      // refetchInterval: 2000,
+      // refetchIntervalInBackground: true,
+      enabled: false, // disable fetch on Mount (to implement controlled fetching as fetch on click using refetch)
+    }
+  );
 
-  if (isLoading) {
+  if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>{error.message}</h2>;
   }
 
   return (
     <>
       <h2>RQSuperHeroesPage</h2>
+      <button onClick={refetch}>Fetch heroes</button>
       {data?.data.map((hero) => {
         return <div key={hero.name}>{hero.name}</div>;
       })}
